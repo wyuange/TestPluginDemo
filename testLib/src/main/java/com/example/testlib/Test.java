02ccbce808b76364e376d3c10f3db98dfc5f96f7
wyuange <3077907364@qq.com>
2020-05-04 06:39:19 +0800

    init pro

---
+++ testLib/src/main/java/com/example/testlib/Test.java
@@ -0,0 +1,17 @@
+package com.example.testlib;
+
+import android.util.Log;
+
+/**
+ * author : wpy
+ * date : 2020-05-03 11:37
+ * desc :
+ */
+public class Test {
+
+    public static void testAd(){
+        Log.e("test","测试返回11111");
+    }
+
+
+}
